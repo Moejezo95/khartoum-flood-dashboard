f1540484761c332031ec106b12e1f2daecd95373
Moejezo95 <mmohamedfadlelseed95@gmail.com>
2025-08-07 23:54:18 +0300

    Update gis_ftl_streamlitapp.py

--- gis_ftl_streamlitapp.py
+++ gis_ftl_streamlitapp.py
@@ -37,7 +37,7 @@ khartoum_gdf = sudan_gdf[sudan_gdf['id'] == 'SDKH']
 # buildings_df = pd.read_csv("/content/drive/My Drive/GIS_FTL_LAST/169_buildings.csv")
 # buildings_df['geometry'] = buildings_df['geometry'].apply(wkt.loads)
 # buildings_gdf = gpd.GeoDataFrame(buildings_df, geometry='geometry', crs='EPSG:4326')
-url = "https://drive.google.com/file/d/1t_wdIU9zDTLeJ0ImEygGARZtqseM0rwa/view?usp=drive_link"
+url = "https://drive.google.com/uc?id=1t_wdIU9zDTLeJ0ImEygGARZtqseM0rwa"
 # url = "https://drive.google.com/uc?id=1t_wdIU9zDTLeJ0ImEygGARZtqseM0rwa"
 buildings_df = pd.read_csv(url) 
 st.subheader("📍 Sample Data")
